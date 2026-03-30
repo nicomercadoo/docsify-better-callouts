d386c1b6980e786579b44e26eb8a2412598f3b29
Nicolás Mercado <nicolasmercado452@gmail.com>
2026-03-30 20:07:31 -0300

    Refactor: md and html processing abstracted into funcitons

--- src/main.js
+++ src/main.js
@@ -24,66 +24,18 @@ import { resolveIcon } from './icons.js';
         hook.beforeEach(function (md) {
             console.debug('Processing markdown for callouts in the page:', vm.route.path);
             console.debug('Original markdown:', md);
-
-            const mdBetterCalloutHeadPattern = new RegExp(`^(?<level>( *>)*) *\\[\\s*!(?<tag>${tagsPattern})(?<ignored>[\\s\\S]*?)\\] ?(?<content>[\\s\\S]*?)$`, 'gm');
-
-            return md.replaceAll(mdBetterCalloutHeadPattern,
-                (...args) => {
-                    console.debug('Found a callout markdown:', args[0]);
-                    const namedCaptureGroups = args.at(-1);
-                    const { level: calloutLevel, tag: calloutTag, ignored: ignoredContentInTag, content: calloutContent } = namedCaptureGroups;
-
-                    let cleanedCalloutHead = `${calloutLevel} [!${calloutTag}]\n${calloutLevel}`;
-                    if (calloutContent) {
-                        /*
-                            It appends '\n${calloutLevel}' again because the first one is needed to break the line after the callout head,
-                            and the second one is needed to ensure that the content of the callout head is rendered as a separate
-                            paragraph inside the blockquote, independent of the content of the callout body.
-                            Then the head is isolated in its own '<p></p>' tags.
-                            Also, this way, it avoids potential empty '<p></p>' tags if the first content of the callout body is not text.
-                        */
-                        cleanedCalloutHead += `\n${calloutLevel} ${calloutContent}`;
-                    }
-                    console.debug('Cleaned callout head markdown:', cleanedCalloutHead);
-
-                    if (ignoredContentInTag) {
-                        console.warn('docsify-better-callouts: Ignored content in head tag:', ignoredContentInTag);
-                    }
-
-                    return cleanedCalloutHead;
-                });
+            return processBetterCalloutsMD(md, tagsPattern, config);
         })
 
         hook.afterEach(function (html) {
             console.debug('Processing callouts in the page:', vm.route.path);
             console.debug('Processing HTML:', html);
 
-            const htmlBetterCalloutsPattern = new RegExp(`<blockquote>\\s*<p>\\s*\\[\\s*!(?<tag>${tagsPattern})\\s*\\]\\s?</p>\\s*(?<content>[\\s\\S]*?)\\s*<\\/blockquote>`, 'g');
+            const htmlDocsifyCalloutPattern = new RegExp(`<blockquote>(?!\\s*<p>\\s*\\[\\s*!(?<tag>${tagsPattern})\\s*\\]\\s?</p>)\\s*(?<content>[\\s\\S]*?)\\s*<\\/blockquote>`, 'g');
 
-            return html.replace(htmlBetterCalloutsPattern,
-                (...args) => {
-                    console.debug('Found a callout:', args[0]);
-                    const namedCaptureGroups = args.at(-1);
-                    const { tag: calloutType,  content: calloutContent } = namedCaptureGroups;
+            let processedHTML = processBetterCalloutsHTML(html, tagsPattern, config);
 
-                    const tagConfig = getTagConfig(calloutType, config);
-
-                    const cssClass = tagConfig.cssClass;
-                    const label = tagConfig.label;
-                    const icon = resolveIcon(tagConfig.icon, config);
-                    console.debug(`Callout tag "${calloutType}" will be rendered with label "${label}", CSS class "${cssClass}", and icon:`, icon);
-
-                    const betterCallout = `<div class="better-callouts ${cssClass}">`
-                        + `<div class="callout-head">`
-                        + `<div class="callout-icon">${icon}</div>`
-                        + `<div class="callout-label">${label}</div>`
-                        + `</div>` // end callout-head
-                        + `<div class="callout-body">${calloutContent}</div>`
-                        + `</div>`; // end better-callouts
-                    console.debug('Generated better callout HTML:', betterCallout);
-
-                    return betterCallout;
-                });
+            return processedHTML;
         });
     }
 
@@ -91,3 +43,62 @@ import { resolveIcon } from './icons.js';
     $docsify = $docsify || {}
     $docsify.plugins = [].concat(betterCalloutsPlugin, $docsify.plugins || [])
 })();
+
+function processBetterCalloutsMD(md, tagsPattern, config) {
+    const mdBetterCalloutHeadPattern = new RegExp(`^(?<level>( *>)*) *\\[\\s*!(?<tag>${tagsPattern})(?<ignored>[\\s\\S]*?)\\] ?(?<content>[\\s\\S]*?)$`, 'gm');
+
+    return md.replaceAll(mdBetterCalloutHeadPattern,
+        (...args) => {
+            console.debug('Found a callout markdown:', args[0]);
+            const namedCaptureGroups = args.at(-1);
+            const { level: calloutLevel, tag: calloutTag, ignored: ignoredContentInTag, content: calloutContent } = namedCaptureGroups;
+
+            let cleanedCalloutHead = `${calloutLevel} [!${calloutTag}]\n${calloutLevel}`;
+            if (calloutContent) {
+                /*
+                    It appends '\n${calloutLevel}' again because the first one is needed to break the line after the callout head,
+                    and the second one is needed to ensure that the content of the callout head is rendered as a separate
+                    paragraph inside the blockquote, independent of the content of the callout body.
+                    Then the head is isolated in its own '<p></p>' tags.
+                    Also, this way, it avoids potential empty '<p></p>' tags if the first content of the callout body is not text.
+                */
+                cleanedCalloutHead += `\n${calloutLevel} ${calloutContent}`;
+            }
+            console.debug('Cleaned callout head markdown:', cleanedCalloutHead);
+
+            if (ignoredContentInTag) {
+                console.warn('docsify-better-callouts: Ignored content in head tag:', ignoredContentInTag);
+            }
+
+            return cleanedCalloutHead;
+        });
+}
+
+function processBetterCalloutsHTML(html, tagsPattern, config) {
+    const htmlBetterCalloutsPattern = new RegExp(`<blockquote>\\s*<p>\\s*\\[\\s*!(?<tag>${tagsPattern})\\s*\\]\\s?</p>\\s*(?<content>[\\s\\S]*?)\\s*<\\/blockquote>`, 'g');
+
+    return html.replaceAll(htmlBetterCalloutsPattern,
+        (...args) => {
+            console.debug('Found a callout:', args[0]);
+            const namedCaptureGroups = args.at(-1);
+            const { tag: calloutType, content: calloutContent } = namedCaptureGroups;
+
+            const tagConfig = getTagConfig(calloutType, config);
+
+            const cssClass = tagConfig.cssClass;
+            const label = tagConfig.label;
+            const icon = resolveIcon(tagConfig.icon, config);
+            console.debug(`Callout tag "${calloutType}" will be rendered with label "${label}", CSS class "${cssClass}", and icon:`, icon);
+
+            const betterCallout = `<div class="better-callouts ${cssClass}">`
+                + `<div class="callout-head">`
+                + `<div class="callout-icon">${icon}</div>`
+                + `<div class="callout-label">${label}</div>`
+                + `</div>` // end callout-head
+                + `<div class="callout-body">${calloutContent}</div>`
+                + `</div>`; // end better-callouts
+            console.debug('Generated better callout HTML:', betterCallout);
+
+            return betterCallout;
+        });
+}
