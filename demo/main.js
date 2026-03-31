67b657884c173e2cfe9afb4d8c54df867dc49f17
Nicolás Mercado <nicolasmercado452@gmail.com>
2026-03-31 03:44:15 -0300

    demo: update

--- demo/main.js
+++ demo/main.js
@@ -4,7 +4,7 @@ var warningIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox
 var defIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M320-160q-33 0-56.5-23.5T240-240v-120h120v-90q-35-2-66.5-15.5T236-506v-44h-46L60-680q36-46 89-65t107-19q27 0 52.5 4t51.5 15v-55h480v520q0 50-35 85t-85 35H320Zm120-200h240v80q0 17 11.5 28.5T720-240q17 0 28.5-11.5T760-280v-440H440v24l240 240v56h-56L510-514l-8 8q-14 14-29.5 25T440-464v104ZM224-630h92v86q12 8 25 11t27 3q23 0 41.5-7t36.5-25l8-8-56-56q-29-29-65-43.5T256-684q-20 0-38 3t-36 9l42 42Zm376 350H320v40h286q-3-9-4.5-19t-1.5-21Zm-280 40v-40 40Z"/></svg>`;
 var tipIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M400-240q-33 0-56.5-23.5T320-320v-50q-57-39-88.5-100T200-600q0-117 81.5-198.5T480-880q117 0 198.5 81.5T760-600q0 69-31.5 129.5T640-370v50q0 33-23.5 56.5T560-240H400Zm0-80h160v-92l34-24q41-28 63.5-71.5T680-600q0-83-58.5-141.5T480-800q-83 0-141.5 58.5T280-600q0 49 22.5 92.5T366-436l34 24v92Zm0 240q-17 0-28.5-11.5T360-120v-40h240v40q0 17-11.5 28.5T560-80H400Zm80-520Z"/></svg>`;
 var dangerIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M330-120 120-330v-300l210-210h300l210 210v300L630-120H330Zm36-190 114-114 114 114 56-56-114-114 114-114-56-56-114 114-114-114-56 56 114 114-114 114 56 56Zm-2 110h232l164-164v-232L596-760H364L200-596v232l164 164Zm116-280Z"/></svg>`;
-function resolveIcon(iconValue, config2) {
+function resolveIcon(iconValue, config) {
   console.debug("Rendering icon for value:", iconValue);
   if (!iconValue || iconValue === "")
     return "";
@@ -15,7 +15,7 @@ function resolveIcon(iconValue, config2) {
   const isUrlOrPath = /^(https?:\/\/|\.?\/)/i.test(iconStr) || /\.(svg|png|jpg|jpeg|gif|webp)$/i.test(iconStr);
   if (isUrlOrPath) {
     const isSvgPath = /\.svg$/i.test(iconStr);
-    if (isSvgPath && config2.svgFileAsRawSvg) {
+    if (isSvgPath && config.svgFileAsRawSvg) {
       return `<div class="svg-file-mask icon" style="-webkit-mask-image: url('${iconStr}'); mask-image: url('${iconStr}');"></div>`;
     }
     return `<img src="${iconStr}" class="icon-img icon" alt="icon"/>`;
@@ -80,32 +80,32 @@ var defaultConfig = {
   processRegularCallouts: true,
   matchLanguageWithCurrentPath: true
 };
-function getTagConfig(tag, config2) {
+function getTagConfig(tag, config) {
   let tagConfig = null;
-  for (const tagPattern of Object.keys(config2.tags)) {
+  for (const tagPattern of Object.keys(config.tags)) {
     if (RegExp(tagPattern).test(tag)) {
-      tagConfig = config2.tags[tagPattern];
+      tagConfig = config.tags[tagPattern];
       break;
     }
   }
   if (!tagConfig) {
     console.warn(`docsify-better-callouts: No configuration found for callout type "${tag}". Using default values.`);
   }
-  return tagConfig || config2.defaultTag;
+  return tagConfig || config.defaultTag;
 }
 function mergeConfig(baseConfig, userConfig, currentPath) {
   checkUserInvalidConfigEntries(baseConfig, userConfig);
   checkUserMissingRequiredConfigEntries(userConfig);
-  let config2 = { ...baseConfig, tags: { ...baseConfig.tags } };
+  let config = { ...baseConfig, tags: { ...baseConfig.tags } };
   const resolvedLang = resolveCurrentLanguage(baseConfig, userConfig, currentPath);
   const languagePack = getLanguagePack(resolvedLang, baseConfig, userConfig);
   if (languagePack) {
-    config2 = applyTagOverrides(config2, languagePack.tags, baseConfig.defaultTag);
+    config = applyTagOverrides(config, languagePack.tags, baseConfig.defaultTag);
   }
   if (userConfig.tags) {
-    config2 = applyTagOverrides(config2, userConfig.tags, baseConfig.defaultTag);
+    config = applyTagOverrides(config, userConfig.tags, baseConfig.defaultTag);
   }
-  return config2;
+  return config;
 }
 function resolveCurrentLanguage(baseConfig, userConfig, currentPath) {
   const shouldMatchPath = userConfig.matchLanguageWithCurrentPath ?? baseConfig.matchLanguageWithCurrentPath;
@@ -140,55 +140,55 @@ function resolveLanguageFromPath(path, availableLangs) {
   }
   return null;
 }
-function applyTagOverrides(config2, overrides, defaultTag2) {
+function applyTagOverrides(config, overrides, defaultTag2) {
   if (!overrides)
-    return config2;
+    return config;
   for (const [userTag, userTagConfig] of Object.entries(overrides)) {
     let matched = false;
-    for (const baseKey of Object.keys(config2.tags)) {
-      if (handleExactMatch(config2, baseKey, userTag, userTagConfig)) {
+    for (const baseKey of Object.keys(config.tags)) {
+      if (handleExactMatch(config, baseKey, userTag, userTagConfig)) {
         matched = true;
         break;
       }
-      if (handleIntersectionMatch(config2, baseKey, userTag, userTagConfig)) {
+      if (handleIntersectionMatch(config, baseKey, userTag, userTagConfig)) {
         matched = true;
         break;
       }
     }
     if (!matched) {
-      handleNewTag(config2, defaultTag2, userTag, userTagConfig);
+      handleNewTag(config, defaultTag2, userTag, userTagConfig);
     }
   }
-  return config2;
+  return config;
 }
-function handleExactMatch(config2, baseKey, userTag, userTagConfig) {
+function handleExactMatch(config, baseKey, userTag, userTagConfig) {
   if (baseKey === userTag) {
-    config2.tags[baseKey] = { ...config2.tags[baseKey], ...userTagConfig };
+    config.tags[baseKey] = { ...config.tags[baseKey], ...userTagConfig };
     return true;
   }
   return false;
 }
-function handleIntersectionMatch(config2, baseKey, userTag, userTagConfig) {
+function handleIntersectionMatch(config, baseKey, userTag, userTagConfig) {
   const baseVariants = baseKey.split("|").map((v) => v.trim());
   const userVariants = userTag.split("|").map((v) => v.trim());
   const intersection = userVariants.filter((v) => baseVariants.includes(v));
   if (intersection.length > 0) {
-    const baseProps = config2.tags[baseKey];
+    const baseProps = config.tags[baseKey];
     const remainingBaseVariants = baseVariants.filter((v) => !intersection.includes(v));
     const remainingBaseKey = remainingBaseVariants.join("|");
-    delete config2.tags[baseKey];
+    delete config.tags[baseKey];
     if (remainingBaseKey !== "") {
-      config2.tags[remainingBaseKey] = baseProps;
+      config.tags[remainingBaseKey] = baseProps;
     }
-    config2.tags[userTag] = { ...baseProps, ...userTagConfig };
+    config.tags[userTag] = { ...baseProps, ...userTagConfig };
     return true;
   }
   return false;
 }
-function handleNewTag(config2, defaultTag2, userTag, userTagConfig) {
+function handleNewTag(config, defaultTag2, userTag, userTagConfig) {
   let newTag = Object.create(defaultTag2);
   Object.assign(newTag, userTagConfig);
-  config2.tags[userTag] = newTag;
+  config.tags[userTag] = newTag;
 }
 function getLanguagePack(lang, baseConfig, userConfig) {
   const userLangs = userConfig.languagePacks ? Object.keys(userConfig.languagePacks) : [];
@@ -242,26 +242,25 @@ function checkUserMissingRequiredConfigEntries(userConfig) {
 // src/main.js
 (function() {
   var betterCalloutsPlugin = function(hook, vm) {
-    let config2;
-    let tagsPattern2;
+    let config;
+    let tagsPattern;
     const userConfig = vm.config.betterCallouts || {};
-    hook.init(function() {
-      refreshConfig(userConfig, vm.route && vm.route.path);
-      console.debug("Config:", config2);
-      console.debug("vm:", vm);
-    });
     hook.beforeEach(function(md) {
-      refreshConfig(userConfig, vm.route && vm.route.path);
+      const currentPath = vm.route && vm.route.path;
+      config = mergeConfig(defaultConfig, userConfig, currentPath);
+      tagsPattern = Object.keys(config.tags).join("|");
+      console.debug("Config:", config);
+      console.debug("vm:", vm);
       console.debug("Processing markdown for callouts in the page:", vm.route.path);
       console.debug("Original markdown:", md);
-      return processBetterCalloutsMD(md, tagsPattern2, config2);
+      return processBetterCalloutsMD(md, tagsPattern, config);
     });
     hook.afterEach(function(html) {
       console.debug("Processing callouts in the page:", vm.route.path);
       console.debug("Processing HTML:", html);
-      let processedHTML = processBetterCalloutsHTML(html, tagsPattern2, config2);
-      if (config2.processRegularCallouts) {
-        processedHTML = processRegularCalloutsHTML(processedHTML, config2);
+      let processedHTML = processBetterCalloutsHTML(html, tagsPattern, config);
+      if (config.processRegularCallouts) {
+        processedHTML = processRegularCalloutsHTML(processedHTML, config);
       }
       return processedHTML;
     });
@@ -269,12 +268,8 @@ function checkUserMissingRequiredConfigEntries(userConfig) {
   $docsify = $docsify || {};
   $docsify.plugins = [].concat(betterCalloutsPlugin, $docsify.plugins || []);
 })();
-function refreshConfig(userConfig, currentPath) {
-  config = mergeConfig(defaultConfig, userConfig, currentPath);
-  tagsPattern = Object.keys(config.tags).join("|");
-}
-function processBetterCalloutsMD(md, tagsPattern2, config2) {
-  const mdBetterCalloutsHeadPattern = new RegExp(`^(?<level>( *>)*) *\\[\\s*!(?<tag>${tagsPattern2})(?<ignored>[\\s\\S]*?)\\] ?(?<content>[\\s\\S]*?)$`, "gm");
+function processBetterCalloutsMD(md, tagsPattern, config) {
+  const mdBetterCalloutsHeadPattern = new RegExp(`^(?<level>( *>)*) *\\[\\s*!(?<tag>${tagsPattern})(?<ignored>[\\s\\S]*?)\\] ?(?<content>[\\s\\S]*?)$`, "gm");
   return md.replaceAll(mdBetterCalloutsHeadPattern, (...args) => {
     console.debug("Found a callout markdown:", args[0]);
     const namedCaptureGroups = args.at(-1);
@@ -292,23 +287,23 @@ ${calloutLevel} ${calloutContent}`;
     return cleanedCalloutHead;
   });
 }
-function processBetterCalloutsHTML(html, tagsPattern2, config2) {
-  const htmlBetterCalloutsPattern = new RegExp(`<blockquote>\\s*<p>\\s*\\[\\s*!(?<tag>${tagsPattern2})\\s*\\]\\s?</p>\\s*(?<content>[\\s\\S]*?)\\s*<\\/blockquote>`, "g");
+function processBetterCalloutsHTML(html, tagsPattern, config) {
+  const htmlBetterCalloutsPattern = new RegExp(`<blockquote>\\s*<p>\\s*\\[\\s*!(?<tag>${tagsPattern})\\s*\\]\\s?</p>\\s*(?<content>[\\s\\S]*?)\\s*<\\/blockquote>`, "g");
   return html.replaceAll(htmlBetterCalloutsPattern, (...args) => {
     console.debug("Found a callout:", args[0]);
     const namedCaptureGroups = args.at(-1);
     const { tag: calloutType, content: calloutContent } = namedCaptureGroups;
-    const tagConfig = getTagConfig(calloutType, config2);
+    const tagConfig = getTagConfig(calloutType, config);
     const cssClass = tagConfig.cssClass;
     const label = tagConfig.label;
-    const icon = resolveIcon(tagConfig.icon, config2);
+    const icon = resolveIcon(tagConfig.icon, config);
     console.debug(`Callout tag "${calloutType}" will be rendered with label "${label}", CSS class "${cssClass}", and icon:`, icon);
     const betterCallout = `<div class="better-callouts ${cssClass}">` + `<div class="callout-head">` + `<div class="callout-icon">${icon}</div>` + `<div class="callout-label">${label}</div>` + `</div>` + `<div class="callout-body">${calloutContent}</div>` + `</div>`;
     console.debug("Generated better callout HTML:", betterCallout);
     return betterCallout;
   });
 }
-function processRegularCalloutsHTML(html, config2) {
+function processRegularCalloutsHTML(html, config) {
   const htmlRegularCalloutsPattern = new RegExp(`<blockquote>(?!\\s*<p>\\s*\\[\\s*![\\s\\S]*?\\s*\\]\\s?</p>)\\s*(?<content>[\\s\\S]*?)\\s*<\\/blockquote>`, "g");
   return html.replaceAll(htmlRegularCalloutsPattern, (...args) => {
     console.debug("Found a regular callout:", args[0]);
@@ -320,5 +315,5 @@ function processRegularCalloutsHTML(html, config2) {
   });
 }
 
-//# debugId=C6EF88BBEF9748ED64756E2164756E21
+//# debugId=1297CE28AFE2303F64756E2164756E21
 //# sourceMappingURL=main.js.map
